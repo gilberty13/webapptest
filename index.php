30dd2f1c256ad979761245a9fe50eaef4fecd5e5
gilberty13 <122580093+gilberty13@users.noreply.github.com>
2023-11-14 08:48:04 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, Azure!</title>
+</head>
+<body>
+    <h1>Hello, Azure!</h1>
+    <p>This is a sample PHP web app deployed to Azure App Service.</p>
+</body>
+</html>
