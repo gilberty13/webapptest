4a5b9f9b170dbb00ec6a15fc62d6f1edd1e81cdc
gilberty13 <122580093+gilberty13@users.noreply.github.com>
2023-11-14 14:34:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hello, Azure!</title>
+    <title>PHP Web App with SQL Server</title>
 </head>
 <body>
-    <h1>Hello, Azure!</h1>
-    <p>This is a sample PHP web app deployed to Azure App Service using PHP 8.0.</p>
+    <h1>PHP Web App with SQL Server</h1>
+    
+    <form method="post">
+        <label for="inputData">Enter Data:</label>
+        <input type="text" name="inputData" id="inputData">
+        <input type="submit" value="Save">
+    </form>
 
     <?php
-    // Database connection code
-    $serverName = "137.116.132.116";
-    $connectionOptions = array(
-        "Database" => "master",
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $serverName = "137.116.132.116";
+        $connectionOptions = array(
+            "Database" => "master",
         "Uid" => "sqladmin",
         "PWD" => "password@123"
-    );
+        );
 
-    // Establish the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
+        // Establish the connection
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-    if (!$conn) {
-        die(print_r(sqlsrv_errors(), true));
+        if (!$conn) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        // Retrieve the user's input data from the form
+        $inputData = $_POST["inputData"];
+
+        // Insert the data into the database
+        $sql = "INSERT INTO UserData (InputData) VALUES (?)";
+        $params = array($inputData);
+        $stmt = sqlsrv_query($conn, $sql, $params);
+
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
+        } else {
+            echo "Data saved successfully!";
+        }
+
+        // Close the connection
+        sqlsrv_close($conn);
     }
-
-    // Query the database
-    $sql = "SELECT * FROM your-table";
-    $query = sqlsrv_query($conn, $sql);
-
-    if ($query === false) {
-        die(print_r(sqlsrv_errors(), true));
-    }
-
-    // Display data from the database
-    echo "<h2>Data from SQL Server:</h2>";
-    echo "<ul>";
-    while ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
-        echo "<li>Name: " . $row['Name'] . "</li>";
-        // Add more fields as needed
-    }
-    echo "</ul>";
-
-    // Close the connection
-    sqlsrv_close($conn);
     ?>
 </body>
 </html>
